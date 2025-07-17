56283ba02ec0163758546ff27b96e72c0535a0a3
Moatasem-x <moatasememad55555@gmail.com>
2025-07-17 05:37:58 +0300

    Created The Repo

---
+++ src/app/Pages/Employee/all-employees/all-employees.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-employees',
+  imports: [],
+  templateUrl: './all-employees.html',
+  styleUrl: './all-employees.css'
+})
+export class AllEmployees {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployee } from './Pages/Employee/add-employee/add-employee';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddEmployee],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
