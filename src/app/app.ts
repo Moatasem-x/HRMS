4cc2967ed60e9f3af0e420f6882dda67261acd11
amrehab690 <amrehab690@gmail.com>
2025-07-22 02:27:58 +0300

    anazheeeee2tyamo3tasem

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,12 @@ import { EmployeeForm } from './Pages/Employee/employee-form/employee-form';
 import { OfficialHolidayCombineComponent } from "./Pages/OfficialHoliday/official-holiday-combine/official-holiday-combine";
 import { AttendanceReportCombineComponent } from "./Pages/AttendanceReport/attendance-report-combine/attendance-report-combine";
 import { EmployeeSalaryCombineComponent } from "./Pages/EmployeeSalary/employee-salary-combine/employee-salary-combine";
+import { DashboardComponent } from "./Pages/dashboard/dashboard";
+import { DashboardModernComponent } from "./Pages/dashboard-modern/dashboard-modern";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeesComponent, EmployeeForm, OfficialHolidayCombineComponent, AttendanceReportCombineComponent, EmployeeSalaryCombineComponent],
+  imports: [RouterOutlet, EmployeesComponent, EmployeeForm, OfficialHolidayCombineComponent, AttendanceReportCombineComponent, EmployeeSalaryCombineComponent, DashboardComponent, DashboardModernComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
